713c5e498684c55af0bbc53406b376db9bf84149
powerlet2020 <powerlet2020@gmail.com>
2024-11-01 12:01:14 +1200

    commite

---
+++ react-css/src/App.jsx
@@ -0,0 +1,14 @@
+import Button from "./Mybutton/button.jsx"
+import Mydiv from "./assets/div.jsx"
+
+
+function App() {
+  return (
+    <>
+    <Button/>
+    <Button/>
+    <Mydiv/>
+    </>
+  )
+}
+export default App
---
+++ react-css/src/assets/div.jsx
@@ -0,0 +1,21 @@
+
+
+function Mydiv(){
+    const styles = {
+        background:'green',
+        color:'white',
+        border:'1px solid black',
+        width:'100px',
+        outline:'none',
+        borderRaius:'10px'
+    }
+
+    return(
+       
+       <div style = {styles} > 
+        <p >my paragraph</p>
+       </div>
+       
+    );
+}
+export default Mydiv
